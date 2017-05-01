9cd1f9a56772e72e8f7c10e421e5ef3f016a37e2
rck-j <rck.jaramillo@gmail.com>
2017-05-01 12:05:19 -0700

    Added URLs for testing

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,12 @@
 import media
 import fresh_tomatoes
 
-pulp_fiction = media.Movie("Pulp Fiction", "Stuff", poster_image_url, trailer_youtube_url)
-lost_translation = media.Movie("Lost in Translation", "Stuff", poster_image_url, trailer_youtube_url)
-wall_e = media.Movie("WALL-E", "Stuff", poster_image_url, trailer_youtube_url)
-matrix = media.Movie("The Matrix", "Stuff", poster_image_url, trailer_youtube_url)
-arrival = media.Movie("Arrival", "Stuff", poster_image_url, trailer_youtube_url)
-o_brother = media.Movie("O Brother, Where Art Thou?", "Stuff", poster_image_url, trailer_youtube_url)
+pulp_fiction = media.Movie("Pulp Fiction", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+lost_translation = media.Movie("Lost in Translation", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+wall_e = media.Movie("WALL-E", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+matrix = media.Movie("The Matrix", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+arrival = media.Movie("Arrival", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+o_brother = media.Movie("O Brother, Where Art Thou?", "Stuff", "https://en.wikipedia.org/wiki/Pulp_Fiction#/media/File:Pulp_Fiction_(1994)_poster.jpg", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
 movies = [pulp_fiction, lost_translation, wall_e, matrix, arrival, o_brother]
 fresh_tomatoes.open_movies_page(movies)

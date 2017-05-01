9d3099651f8d7057d5ea82930848780bac6811c3
rck-j <rck.jaramillo@gmail.com>
2017-05-01 15:27:10 -0700

    Super messy

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,36 +2,38 @@ import media
 import fresh_tomatoes
 
 #Create Movie objects
-pulp_fiction = media.Movie("Pulp Fiction",
-                           "https://upload.wikimedia.org/wikipedia/" \
-                           "en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
-                           "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
-
-lost_translation = media.Movie("Lost in Translation",
-                               "https://upload.wikimedia.org/wikipedia/" \
-                               "en/4/4c/Lost_in_Translation_poster.jpg",
-                               "https://www.youtube.com/watch?v=sU0oZsqeG_s")
-
-wall_e = media.Movie("WALL-E",
-                     "https://upload.wikimedia.org/wikipedia" \
-                     "/en/c/c2/WALL-Eposter.jpg",
-                     "https://www.youtube.com/watch?v=ZisWjdjs-gM")
-
-matrix = media.Movie("The Matrix",
-                     "https://upload.wikimedia.org/wikipedia/" \
-                     "en/c/c1/The_Matrix_Poster.jpg",
-                     "https://www.youtube.com/watch?v=Q8g9zL-JL8E")
-
-arrival = media.Movie("Arrival",
-                      "https://upload.wikimedia.org/wikipedia/" \
-                      "en/d/df/Arrival%2C_Movie_Poster.jpg",
-                      "https://www.youtube.com/watch?v=tFMo3UJ4B4g")
-
-o_brother = media.Movie("O Brother, Where Art Thou?",
-                        "https://upload.wikimedia.org/wikipedia/" \
-                        "en/5/5b/O_brother_where_art_thou_ver1.jpg",
-                        "https://www.youtube.com/watch?v=eW9Xo2HtlJI")
+# pulp_fiction = media.Movie("Pulp Fiction",
+#                            "https://upload.wikimedia.org/wikipedia/" \
+#                            "en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+#                            "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+#
+# lost_translation = media.Movie("Lost in Translation",
+#                                "https://upload.wikimedia.org/wikipedia/" \
+#                                "en/4/4c/Lost_in_Translation_poster.jpg",
+#                                "https://www.youtube.com/watch?v=sU0oZsqeG_s")
+#
+# wall_e = media.Movie("WALL-E",
+#                      "https://upload.wikimedia.org/wikipedia" \
+#                      "/en/c/c2/WALL-Eposter.jpg",
+#                      "https://www.youtube.com/watch?v=ZisWjdjs-gM")
+#
+# matrix = media.Movie("The Matrix",
+#                      "https://upload.wikimedia.org/wikipedia/" \
+#                      "en/c/c1/The_Matrix_Poster.jpg",
+#                      "https://www.youtube.com/watch?v=Q8g9zL-JL8E")
+#
+# arrival = media.Movie("Arrival",
+#                       "https://upload.wikimedia.org/wikipedia/" \
+#                       "en/d/df/Arrival%2C_Movie_Poster.jpg",
+#                       "https://www.youtube.com/watch?v=tFMo3UJ4B4g")
+#
+# o_brother = media.Movie("O Brother, Where Art Thou?",
+#                         "https://upload.wikimedia.org/wikipedia/" \
+#                         "en/5/5b/O_brother_where_art_thou_ver1.jpg",
+#                         "https://www.youtube.com/watch?v=eW9Xo2HtlJI")
 
 #Crete list of movies to be passed to fresh_tomatoes function open_movies_page
-movies = [pulp_fiction, lost_translation, wall_e, matrix, arrival, o_brother]
+#movies = [pulp_fiction, lost_translation, wall_e, matrix, arrival, o_brother]
+pulp_fiction = media.Movie("the matrix", "https://www.youtube.com/watch?v=Q8g9zL-JL8E")
+movies = [pulp_fiction]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,12 +1,26 @@
-import webbrowser
+import urllib
+import ast
 
+# class Movie():
+#
+#     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+#         self.title = movie_title
+#         self.poster_image_url = poster_image_url
+#         self.trailer_youtube_url = trailer_youtube_url
+
+
+# Going to and get movie information from omdb api
 class Movie():
-    """docstring for Movie."""
-    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_query, trailer_youtube_url):
+        omdb_query_string = movie_query.replace(" ", "+")
+        website = "http://www.omdbapi.com/?t="+omdb_query_string
+        print(website)
+        proxy = "http://10.32.1.8:10149"
+        connection = urllib.urlopen(website, proxies = {'http': proxy})
+        output = connection.read()
+        print(output)
+        dict_output = ast.literal_eval(output)
+        self.title = dict_output["Title"]
+        self.poster_image_url = dict_output["Poster"]
+        self.trailer_youtube_url = trailer_youtube_url
